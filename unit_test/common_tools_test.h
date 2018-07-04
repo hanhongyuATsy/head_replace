4fbaf8b158818e7e26289d7314b5b5f48d156af3
hhy <scuhanhongyu@sina.com>
2018-07-04 11:23:08 +0800

    add commont_tools_test.h

---
+++ unit_test/common_tools_test.h
@@ -0,0 +1,38 @@
+#ifndef _COMMON_TOOLS_TEST_H
+#define _COMMON_TOOLS_TEST_H
+
+#include "gtest/gtest.h"
+#include "../common_tools.h"
+#include "filenamereader.h"
+#include "filetxtreader.h"
+
+using namespace std;
+
+class ListFileValidationTest: public testing::Test
+{
+
+protected:
+    void SetUp();
+    void TearDown();
+
+public:
+    /**
+     * @brief     测试ListFile::get_all_file_count()
+     * @param[in]
+     * @param[out]
+     * @return
+     */
+    void get_all_file_count_Test();
+
+private:
+    void parseOperatorParam(const string& fileName,string &file_cnt,string &dir_name);
+    // 读取测试数据文件名
+    FileNameReader _fileNameReader;
+    // 解析测试数据
+    FileTxtReader _fileTxtReader;
+
+    vector<string> _vecOperatorFileName;
+
+};
+
+#endif
